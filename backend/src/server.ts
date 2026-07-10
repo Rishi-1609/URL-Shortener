471b0a1b54c0433714db5d9cc657279b1b032eee
rishindra <raorishindra16@gmail.com>
2026-07-10 22:49:48 +0530

    added database dependencies

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,3 +4,37 @@ import { env } from "./config/env.js";
 app.listen(env.PORT, ()=> {
     console.log(`Server listening at http://localhost:${env.PORT}`);
 })
+
+interface reqBody {
+    originalUrl : string;
+    expiresAt? : Date;
+}
+
+interface successResponse {
+    success : boolean;
+    data : {
+        shortCode : string;
+        shortUrl : string;
+        expiresAt : Date;
+        createdAt : Date;
+    }
+}
+
+interface validationError {
+    success : boolean;
+    error : {
+        msg : string;
+        issues : {}
+    }
+}
+
+interface urlDbSchema {
+    id : string;
+    userId : string;
+    originalUrl : string;
+    shortUrl : string;
+    expiresAt : Date;
+    createdAt : Date;
+    updatedAt : Date;
+    clickCount : Number;
+}
